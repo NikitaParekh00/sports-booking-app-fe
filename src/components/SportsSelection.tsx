f976cb34cb421095e6fe57abb0da4199559b2e10
Harshita Kala <nikitaparekh00@gmail.com>
2025-10-03 22:02:07 +0530

    fix: address fix upto 4 words

--- src/components/SportsSelection.tsx
+++ src/components/SportsSelection.tsx
@@ -200,7 +200,7 @@ export default function SportsSelection({ selectedLocation = "Rajendra Nagar", o
               <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
               </svg>
-              <span className="text-sm">{selectedLocation}</span>
+              <span className="text-sm">{selectedLocation.split(' ').slice(0, 4).join(' ')}</span>
               <button
                 onClick={() => {
                   if (onRequestLocationChange) {
